d2801256fa9805815bcef3ec2e698ce6db821cef
yelikang <530331059@qq.com>
2023-03-10 14:58:35 +0800

    引入备注书写

--- src/main.js
+++ src/main.js
@@ -11,13 +11,14 @@ import App from './App.vue'
 
 //3.组件发布阶段=全量引入(需引入全局css)
 // import components from 'lib'
-// import 'lib/style'
+// import 'lib/theme/index.css'
+// Vue.use(components)
 
 //4.组件发布阶段=按需引入(需引入个性css,或项目中结合babel-plugin-import做引入)
-// 自行引入
+// 4-1 自行引入
 // import {CInput} from 'lib'
-// import 'lib/c-input/style'
-// babel-plugin-import 引入(只需要引入对应的组件包即可，style由import插件引入)
+// import 'lib/theme/style'
+// 4-2 babel-plugin-import 引入(只需要引入对应的组件包即可，style由import插件引入)
 // import {CInput} from 'lib'
 
 
